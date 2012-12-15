9919b4ed1edf9254b498954887c88ccb3a4b1995
Guillermo <gjulian93@gmail.com>
2012-12-15 19:03:02 +0100

    Game engine with missing files :P

---
+++ GameBuilder/GameBuilder/BuilderPage.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using GameBuilder.Definition;
+using GameBuilder.Helpers;
+using System.Diagnostics;
+using Microsoft.Xna.Framework.Input.Touch;
+
+namespace GameBuilder
+{
+    public partial class BuilderPage : PhoneApplicationPage
+    {
+        ContentManager contentManager;
+        GameTimer timer;
+        SpriteBatch spriteBatch;
+        public static GameDefinition Definition { get; set; }
+
+        public BuilderPage()
+        {
+            InitializeComponent();
+
+            // Obtenga el administrador de contenido de la aplicación
+            contentManager = (Application.Current as App).Content;
+
+            // Crear un temporizador para esta página
+            
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            // Establecer el modo de uso compartido del dispositivo gráfico para que active una representación de XNA
+            SharedGraphicsDeviceManager.Current.GraphicsDevice.SetSharingMode(true);
+
+            // Cree un nuevo objeto SpriteBatch, que puede usarse para dibujar texturas.
+            spriteBatch = new SpriteBatch(SharedGraphicsDeviceManager.Current.GraphicsDevice);
+
+            // Iniciar el temporizador
+            timer.Start();
+
+            base.OnNavigatedTo(e);
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            // Detener el temporizador
+            timer.Stop();
+
+            // Establecer el modo de uso compartido del dispositivo gráfico para que desactive una representación de XNA
+            SharedGraphicsDeviceManager.Current.GraphicsDevice.SetSharingMode(false);
+
+            base.OnNavigatedFrom(e);
+        }
+
+        /// <summary>
+        /// Permite que la página ejecute la lógica para, por ejemplo, actualizar el entorno,
+        /// comprobar colisiones, recopilar datos de entrada y reproducir audio.
+        /// </summary>
+        private void OnUpdate(object sender, GameTimerEventArgs e)
+        {
+        }
+
+        /// <summary>
+        /// Permite a la página dibujarse a sí misma.
+        /// </summary>
+        private void OnDraw(object sender, GameTimerEventArgs e)
+        {  
+            spriteBatch.Begin();
+           
+            spriteBatch.End();
+        }
+    }
+}
---
+++ GameBuilder/GameBuilder/Definition/Player.cs
@@ -0,0 +1,20 @@
+﻿using GameBuilder.Helpers;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GameBuilder.Definition
+{
+    public class Player : GameObject
+    {
+        public Texture2D Texture { get; set; }
+       
+        public Player(GraphicsDevice device, int blocksize)
+        {
+            Texture = ColorTextureCreator.Create(device, blocksize, blocksize, Color.DarkGoldenrod);
+        }
+    }
+}
